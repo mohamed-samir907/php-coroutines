c96ebc25fbfe2b22a589ccde2682d1a527623288
Mohamed Samir <gm.mohamedsamir@gmail.com>
2021-01-24 05:10:34 +0200

    Add basic Coroutine class

---
+++ examples/index.php
@@ -0,0 +1,51 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use MoSamirzz\Coroutine\Coroutine as Co;
+
+$before = microtime(true);
+
+Co::create(function() {
+    Co::delay(2000, function() {
+        echo "World\n";
+    });
+
+    Co::delay(1000, function() {
+        echo "Hello\n";
+    });
+
+    Co::go(function() {
+        $gen = function() {
+            yield 1;
+            yield 2;
+            yield 3;
+        };
+
+        $gen = $gen();
+
+        foreach ($gen as $g) {
+            echo $g . "\t";
+        }
+
+        echo "\n";
+    });
+
+    Co::go(function() {
+        echo time() . "\n";
+    });
+
+    Co::go(function() {
+        echo time() . "\n";
+    });
+
+    Co::go(function() {
+        echo time() . "\n";
+    });
+
+    Co::go(function() {
+        echo time() . "\n";
+    });
+});
+
+echo PHP_EOL . "Time: " . (microtime(true) - $before) . PHP_EOL;
