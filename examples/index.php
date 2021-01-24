e9e6ddbb505ff33444c6b1567c1c4d9eb335864d
Mohamed Samir <gm.mohamedsamir@gmail.com>
2021-01-24 05:29:20 +0200

    Edit example and fix Resolve error

--- examples/index.php
+++ examples/index.php
@@ -5,24 +5,17 @@ require __DIR__ . '/../vendor/autoload.php';
 use MoSamirzz\Coroutine\Coroutine as Co;
 
 $before = microtime(true);
-
 Co::create(function() {
-    Co::delay(2000, function() {
-        echo "World\n";
-    });
+    Co::delay(2000, function() { echo "World\n"; });
 
-    Co::delay(1000, function() {
-        echo "Hello\n";
-    });
+    Co::delay(1000, function() { echo "Hello\n"; });
 
     Co::go(function() {
-        $gen = function() {
+        $gen = (function() {
             yield 1;
             yield 2;
             yield 3;
-        };
-
-        $gen = $gen();
+        })();
 
         foreach ($gen as $g) {
             echo $g . "\t";
@@ -31,21 +24,10 @@ Co::create(function() {
         echo "\n";
     });
 
-    Co::go(function() {
-        echo time() . "\n";
-    });
-
-    Co::go(function() {
-        echo time() . "\n";
-    });
-
-    Co::go(function() {
-        echo time() . "\n";
-    });
-
-    Co::go(function() {
-        echo time() . "\n";
-    });
+    Co::go(function() { echo time() . "\n"; });
+    Co::go(function() { echo time() . "\n"; });
+    Co::go(function() { echo time() . "\n"; });
+    Co::go(function() { echo time() . "\n"; });
 });
 
 echo PHP_EOL . "Time: " . (microtime(true) - $before) . PHP_EOL;
